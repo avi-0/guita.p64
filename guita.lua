504da678f3ba0de6db1225a72d3c048dd4b36e46
Leonid Gordenin <db0zmail@gmail.com>
2024-10-02 16:47:50 -0300

    optimizing textwrap

--- guita.lua
+++ guita.lua
@@ -1,39 +1,131 @@
---[[pod_format="raw",created="2024-09-29 19:59:14",modified="2024-10-02 17:21:43",revision=237]]
+--[[pod_format="raw",created="2024-09-29 19:59:14",modified="2024-10-02 19:47:31",revision=577]]
 guita = {}
 
-function textwrap(str, width)
-     -- printing offscreen to get line width
-    local OFFSCREEN = 480
-    
-    local lines = {}
-    local current_line = ""
-    
-    local x, y = OFFSCREEN, 0
-    for word in str:gmatch("([^%s]+%s*)") do
-        x = print(word, x, y)
-        if x > OFFSCREEN + width then
-            add(lines, current_line)
-            current_line = ""
-    
-            x = OFFSCREEN
-            x = print(word, x, y)
-        end
-        
-        current_line ..= word
-    end
-    
-    add(lines, current_line)    
+function guita.memo(f)
+	local last_params = {}
+	local last_values = {}
+	
+	return function(...)
+		for i,v in pairs({...}) do
+			if last_params[i] != v then
+				last_params = pack(...)
+				last_values = pack(f(...))
+				break
+			end
+		end
+		return unpack(last_values)
+	end
+end
 
-    return lines
+local textlength = function(str)
+	profile("textlength")
+	
+	local p = print(str,480,0)
+	
+
+	profile("textlength")
+	return p-480
+end
+
+textwrapcalls = 0 -- debug
+local function textwrap(str, width)
+	profile("textwrap")
+	textwrapcalls += 1
+	
+	local lines = {}
+	local line_widths = {}
+	
+	local current_line = ""
+	local current_width = 0
+	
+	local x = 0
+	for wordspace in str:gmatch("([^%s]+%s*)") do
+		local word, space = wordspace:match("^([^%s]+)(%s*)$")
+		
+		x += textlength(word)
+		if x > width then
+			add(lines, current_line)
+			add(line_widths, current_width)
+			current_line = ""
+			current_width = 0
+			
+			x = 0
+			x += textlength(wordspace)
+			
+			current_line ..= wordspace
+			current_width = x
+		else
+			current_line ..= word
+			current_width = x
+			
+			x += textlength(space)
+			
+			if x > width then
+				add(lines, current_line)
+				add(line_widths, current_width)
+				current_line = ""
+				current_width = 0
+				
+				x = 0
+			else
+				current_line ..= space
+				--current_width = x - OFFSCREEN
+				--if it's last space in a line - shouldnt count
+				--if there's another word in the line
+				-- - current_width will get updated anyway
+			end
+		end
+		
+		-- check if a newline character was there
+		if space:match("\n") then
+			add(lines, current_line)
+			add(line_widths, current_width)
+			current_line = ""
+			current_width = 0
+			
+			x = 0
+		end
+	end
+	add(lines, current_line)
+	add(line_widths, current_width)
+	
+	profile("textwrap")
+
+	return lines, line_widths
 end
 
 function guita.text(el)
+	el.text = el.text or ""
+
+	el.text_justify = el.text_justify or "left"
+	
+	local memo_textwrap = guita.memo(textwrap)
+	
+	el.layout = el.layout or {}
+	el.layout.request_size = function(width, height)
+		local lines, line_widths = memo_textwrap(el.text, width + 1)
+		
+		local req_width = width
+		local req_height = 8 * #lines
+		
+		return req_width, req_height
+	end
+
 	el.draw = function(self)
 		rectfill(0, 0, self.width, self.height, 7)
 		
-		local lines = textwrap(el.text, self.width - 4)
+		local lines, line_widths = memo_textwrap(el.text, self.width + 1)
 		for i = 1, #lines do
-			print(lines[i],2,2+(i-1)*8,0)
+			local x
+			if el.text_justify == "left" then
+				x = 0
+			elseif el.text_justify == "center" then
+				x = (self.width + 1)/2 - line_widths[i] / 2
+			elseif el.text_justify == "right" then
+				x = self.width + 1 - line_widths[i]
+			end
+			
+			print(lines[i],x,(i-1)*8,0)
 		end
 	end
 	
@@ -60,23 +152,78 @@ function guita.box(el)
 	
 	el.gap = el.gap or 0
 	
-	local available = function()
-		local gaps = el.gap * (#el.child - 1)	
-
-		if el.layout == "row" then
-			return el.width - el.pl - el.pr - gaps
-		elseif el.layout == "column" then
-			return el.height - el.pt - el.pb - gaps
+	local child_requested = function(child)
+		if child.layout and child.layout.request_size then
+			local av_w = el.width - el.pl - el.pr
+			local av_h = el.height - el.pt - el.pb
+			local req_width, req_height = child.layout.request_size(av_w, av_h)
+			
+			if el.layout == "row" then
+				return req_width
+			elseif el.layout == "column" then
+				return req_height
+			end
+		else
+			return 0
 		end
 	end
 	
-	local per_child = function()
-		return available() / #el.child
+	local available = function()
+		local gaps = el.gap * (#el.child - 1)
+		
+		local amt	
+
+		if el.layout == "row" then
+			amt = el.width - el.pl - el.pr - gaps
+		elseif el.layout == "column" then
+			amt = el.height - el.pt - el.pb - gaps
+		end
+		
+		for child in all(el.child) do
+			amt -= child_requested(child)
+		end
+		
+		--amt = math.max(0, amt)
+		
+		return amt
 	end
 	
+	local child_weight = function(child, av)
+		if child.layout then
+			local wt = child.layout.weight or 0
+			if av < 0 then
+				wt = math.max(1, wt)
+			end
+			
+			return wt
+		else
+			return 1
+		end
+	end
+	
+	local total_weight = function(av)
+		local total = 0
+		
+		for child in all(el.child) do
+			total += child_weight(child, av)
+		end
+		
+		return total
+	end
+	
+	local per_child = function(av)
+		return av / total_weight(av)
+	end
+	
+	local child_size = function(child, av)
+		return child_requested(child) + child_weight(child, av) * per_child(av)
+	end	
+
 	el.update = function(self)
+		local av = available()
+
+		local delta = 0
 		for i, child in pairs(self.child) do
-			local delta = (per_child() + self.gap) * (i - 1)
 			local dx = self.layout == "row" and delta or 0
 			local dy = self.layout == "column" and delta or 0
 	
@@ -85,11 +232,14 @@ function guita.box(el)
 			child.width = el.width - el.pl - el.pr
 			child.height = el.height - el.pt - el.pb
 			
-			if self.layout == "row" then
-				child.width = per_child()
-			elseif self.layout == "column" then
-				child.height = per_child()
+			local size = child_size(child, av)
+			if el.layout == "row" then
+				child.width = size
+			elseif el.layout == "column" then
+				child.height = size
 			end
+			
+			delta += size + el.gap
 		end
 	end
 	
--- main.lua
+++ main.lua
@@ -1,22 +1,38 @@
---[[pod_format="raw",created="2024-09-29 19:42:25",modified="2024-10-02 17:21:43",revision=230]]
+--[[pod_format="raw",created="2024-09-29 19:42:25",modified="2024-10-02 19:47:31",revision=507]]
 include "guita.lua"
+include "profiler.lua"
+
+profile.enabled(true, true)
 
 gui = create_gui()
 window(320,240)
 
+local test_text = "btnp is short for \"Button Pressed\"; \nInstead of being true when a button is held down, btnp returns true when a button is down and it was not down the last frame. It also repeats after 30 frames, returning true every 8 frames after that. This can be used for things like menu navigation or grid-wise player movement."
+
 local box = gui:attach(guita.box {p = 4, gap = 4, layout = "column"})
 box:attach(guita.text {
-	text = "btnp is short for \"Button Pressed\"; Instead of being true when a button is held down, btnp returns true when a button is down and it was not down the last frame. It also repeats after 30 frames, returning true every 8 frames after that. This can be used for things like menu navigation or grid-wise player movement.",
+	text = test_text,
+--	layout = {weight = 1},
 })
 box:attach(guita.text {
-	text = "btnp is short for \"Button Pressed\"; Instead of being true when a button is held down, btnp returns true when a button is down and it was not down the last frame. It also repeats after 30 frames, returning true every 8 frames after that. This can be used for things like menu navigation or grid-wise player movement.",
+	text = test_text,
+	text_justify = "center",
+--	layout = {weight = 1},
 })
 box:attach(guita.text {
-	text = "btnp is short for \"Button Pressed\"; Instead of being true when a button is held down, btnp returns true when a button is down and it was not down the last frame. It also repeats after 30 frames, returning true every 8 frames after that. This can be used for things like menu navigation or grid-wise player movement.",
+	text = test_text,
+	text_justify = "right",
+--	layout = {weight = 1},
 })
 
 function _draw()
+	cls(15)
 	gui:draw_all()
+	
+	--print("\#0" .. stat(1), 1, 1, 8)
+	profile.draw()
+	--print("\#0" .. textwrapcalls, 1, 1, 8)
+	
 end
 
 function _update()
@@ -25,6 +41,8 @@ function _update()
 	box.y = 0
 	box.width = width
 	box.height = height
+
+	textwrapcalls = 0
 
 	gui:update_all()
 end
